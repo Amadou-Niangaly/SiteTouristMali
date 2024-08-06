d3eabdee2bf12a1bab27e582feda3768c05f2e77
Amadou Niangaly <amadouniangaly16@gmail.com>
2024-08-06 17:58:31 +0000

    commit 3

--- src/app/zone-tourist-list/zone-tourist-list.component.ts
+++ src/app/zone-tourist-list/zone-tourist-list.component.ts
@@ -4,17 +4,19 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { DetailsComponent } from "../details/details.component";
 import { ZoneTouristService } from '../zone-tourist.service';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-zone-tourist-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, DetailsComponent],
+  imports: [CommonModule, RouterModule, DetailsComponent,NgxPaginationModule],
   templateUrl: './zone-tourist-list.component.html',
   styleUrl: './zone-tourist-list.component.css'
 })
 export class ZoneTouristListComponent {
   @Input() zoneTourist!:ZoneTourist;
   villeList:ZoneTourist[]=[];
+  p:number=1;
   constructor(private zoneTouristService:ZoneTouristService){
     this.villeList=this.zoneTouristService.getAllZoneTouristList();
   }
